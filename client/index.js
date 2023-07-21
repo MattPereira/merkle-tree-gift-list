6208244f9daa30d8a8d30622588e5356d35d5d04
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-07-21 10:33:52 -0700

    complete project

--- client/index.js
+++ client/index.js
@@ -1,14 +1,24 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+  // TODO: how do we prove to the server we're on the nice list?
+  // send a proof and name to the server through the body of the post request
+
+  // create the merkle tree for the whole nice list
+  const merkleTree = new MerkleTree(niceList);
+
+  const name = "Matt Pereira";
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
+  console.log("proof", proof);
 
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    name,
+    proof,
   });
 
   console.log({ gift });
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
 
 const port = 1225;
 
@@ -8,18 +8,25 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT =
+  "f1531cce71851a41330624e1aa20dfd9d0c1f0347120f5122683d801d074c56a";
 
-app.post('/gift', (req, res) => {
+/**
+ *  the Merkle Root is a concise way to represent a whole set of data
+ *  a Merkle Proof uses the properties of the Merkle tree to prove
+ *  that a specific piece of data (name)
+ *  is part of the set represented by the Merkle Root
+ */
+
+app.post("/gift", (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { name, proof } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  const isInTheList = verifyProof(proof, name, MERKLE_ROOT);
+
+  if (isInTheList) {
     res.send("You got a toy robot!");
-  }
-  else {
+  } else {
     res.send("You are not on the list :(");
   }
 });
